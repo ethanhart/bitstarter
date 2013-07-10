510edab673c3c3ed74488dbd53954bc72dfc0a6a
Ethan Hart <ethan.john.hart@gmail.com>
2013-07-10 19:07:31 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+var display_text = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(display_text);
 });
 
 var port = process.env.PORT || 5000;
